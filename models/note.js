a9a9e5b095aad35a30747b18e13f1bd19937509b
ejpelagio <ejpelagio@yahoo.com>
2017-01-11 00:12:01 -0600

    Note schema

---
+++ models/note.js
@@ -0,0 +1,23 @@
+// Require Mongoose
+var mongoose = require('mongoose');
+//Create Schema class
+var Schema = mongoose.Schema;
+
+
+// Creating the Note schema
+var NoteSchema = new Schema({
+
+  title: {
+    type:String
+  },
+ 
+  body: {
+    type:String
+  }
+});
+
+//Create the Note model with the NoteSchema.
+var Note = mongoose.model('Note', NoteSchema);
+
+// Exporting the Note model
+module.exports = Note;
