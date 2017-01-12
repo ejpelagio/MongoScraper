279495020c93d705a13a45443c3f51513efae9c0
ejpelagio <ejpelagio@yahoo.com>
2017-01-11 23:35:39 -0600

    Jquery main.js update

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,8 +1,8 @@
-// grab the articles as a json
+// Grab the articles as a json
 $.getJSON('/articles', function(data) {
-  // for each one
+  // For each one
   for (var i = 0; i<data.length; i++){
-    // display the apropos information on the page
+    // Display the apropos information on the page
     $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ '<a href="' + data[i].link  + '"' + 'target="' + "_blank" +' " >' +  data[i].link + '</a>' + '</p>');
   }
 });
@@ -11,7 +11,7 @@ $.getJSON('/articles', function(data) {
 // When someone clicks a <h3> tag
 $(document).on('click', 'h3', function(){
 
-  // empty the notes from the note section
+  // Empty the notes from the note section
   $('#results').empty();
  // Save the id from the <h3> tag
   var articleID = $(this).attr('data-id');
@@ -25,4 +25,24 @@ $(document).on('click', 'h3', function(){
     .done(function( data ) {
       //Console.log my data
       console.log(data);
-     
+      // The title of the article
+      $('#results').append('<h3>' + data.title + '</h3>');
+      // An input to enter a new title
+      $('#results').append('<input id="titleinput" name="title" >');
+      // Textarea to add a new Note.
+      $('#results').append('<textarea id="bodyinput" name="body"></textarea>');
+      // A button to submit a new note, with the id of the article saved to it
+      $('#results').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
+      // If there's a note in the article
+      if(data.note){
+        console.log(data.note);
+        // A button to delete the notes on the article
+      $('#results').append('<button data-id="' + data.note._id + '" id="deletenote">Delete Note</button>');
+        // Place the title of the note in the title input
+        $('#titleinput').val(data.note.title);
+        // Place the body of the note in the body textarea
+        $('#bodyinput').val(data.note.body);
+      }
+    });
+});
+
