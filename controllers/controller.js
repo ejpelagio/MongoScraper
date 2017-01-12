ece4e67a68cb2352d80e2911136eba5a6017c07b
ejpelagio <ejpelagio@yahoo.com>
2017-01-11 21:10:50 -0600

    Remainder of the  routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -72,3 +72,82 @@ router.get('/home', function(req, res) {
     res.render('index.handlebars', handleObj);
   });
 });
+//This will grab the articles we scraped.
+router.get('/articles', function(req, res) {
+	// Grab every doc from the Article array
+    Article.find({}, function(err, doc) {
+        if (err) {
+        	// Consolog any errors or log the doc
+            console.log(err);
+        }
+        else {
+            res.json(doc);
+        }
+    });
+});
+//.get Articles by it's objectId
+router.get('/articles/:id', function(req, res) {
+    //Using the id passed in the id parameter and find the matching one in our DB.
+    Article.findOne({ '_id': req.params.id })
+        // Populate all of the notes.
+        .populate('Note')
+        // Execute our query
+        .exec(function(err, doc) {
+            if (err) {
+            	// Consolog any errors or log the doc
+                console.log(err);
+            } else {
+                res.json(doc);
+            }
+        });
+});
+//Create a newNote
+router.post('/articles/:id', function(req, res) {
+    // Create a new note and pass the req.body entry.
+    var newNote = new Comment(req.body);
+    //Save the new Note to the DB.
+    newNote.save(function(err, doc) {
+        if (err) {
+        	// Consolog any errors 
+            console.log(err);
+        } 
+        	// or
+        else {
+        	//Using the ArticleId passed in the URL,
+        	//Find the matching ArticleId in our DB,
+        	//Update it
+            Article.findOneAndUpdate({ '_id': req.params.id }, { 'Note': doc._id })
+                .exec(function(err, doc) {
+                    if (err) {
+                    	// Consolog any errors or log the doc
+                        console.log(err);
+                    } else {
+                        res.send(doc);
+                    }
+                });
+        }
+    });
+});
+
+// this function is my delete notes.
+router.post('/deletenote/:id', function(req, res) {
+    // Using the id passed the id parameter,
+    // Prepare a query that finds ID in our DB
+    console.log(req.params.id);
+    Note.findOne({ '_id': req.params.id })
+        // Populate all of the notes.
+        .remove('note')
+        // Executing our query
+        .exec(function(err, doc) {
+            // log any errors
+            if (err) {
+                console.log(err);
+            }
+            //Send doc to browser
+            else {
+                res.json(doc);
+            }
+        });
+});
+
+module.exports = router;
