6b734f07e3dc49809537510c3c361c54f104d752
ejpelagio <ejpelagio@yahoo.com>
2017-01-11 21:10:09 -0600

    Scraping routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -14,6 +14,7 @@ var mongoose = require('mongoose');
 var cheerio = require('cheerio');
 
 // Database configuration with Mongoose
+mongoose.connect('mongodb://heroku_8wf2llsk:6jcbt1d2f95lk6f6308p3vgiha@ds161028.mlab.com:61028/heroku_8wf2llsk');
 var db = mongoose.connection;
 
 // Show any Mongoose 'error'
@@ -24,4 +25,50 @@ db.on('error', function(err) {
 //than console.log the success message
 db.once('open', function() {
   console.log('Mongoose connection successful.');
+});
+
+// Relative href values
+var START_URL = "http://www.foxnews.com/sports.html";
+var url = new URL(START_URL);
+var baseUrl = url.protocol + "//" + url.hostname;
+
+router.get('/', function(req, res) {
+  res.redirect('/home');
+});
+
+router.get('/home', function(req, res) {
+  // First step, We grab the body of the html with request
+  request('http://www.foxnews.com/sports.html', function(error, response, html) {
+  	// Then we load that into cheerio and save it.
+    var $ = cheerio.load(html);
+    var resultsArray = [];
+     // Here we are going to grab every <h3> article tag.
+    $('h3').each(function(i, element) {
+    			//empty result object called 
+				var result = {};
+
+				// Add the text and href of everylink and save them as properties.	
+				result.title = $(this).children('a').text();
+				result.link = baseUrl + $(this).children('a').attr('href');
+
+        			resultsArray.push(result);
+        			// Using our Article model and create a new entry.
+					var entry = new Article (result);
+					// Now, save that entry to the db
+					entry.save(function(err, doc) {
+					  if (err) {
+					  	// Consolog any errors or log the doc
+					    console.log(err);
+					  }
+					  else {
+					    console.log(doc);
+					  }
+				});
+					 res.render('home');// Complete Scraping Text.
+    	});
+
+    var handleObj = {articles: resultsArray};
+    console.log(handleObj);
+    res.render('index.handlebars', handleObj);
+  });
 });
